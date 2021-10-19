9cd93b1f9895744afefaaf9354be3f9b7884a450
CraDeed <56147655+CraDeed@users.noreply.github.com>
2021-10-19 18:14:32 +0900

    useInput

---
+++ front/src/hooks/useInput.tsx
@@ -0,0 +1,19 @@
+import React, { useCallback, useState } from 'react';
+
+const useInput = (
+  initialValue: string,
+): [
+  string,
+  React.ChangeEventHandler<HTMLInputElement>,
+  React.Dispatch<React.SetStateAction<string>>,
+] => {
+  const [value, setValue] = useState<string>(initialValue);
+
+  const handler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  }, []);
+
+  return [value, handler, setValue];
+};
+
+export default useInput;
